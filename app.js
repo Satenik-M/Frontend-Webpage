b474340678e5a2841f621b20075c96a6a0507d88
Satenik-M <satenik@markosyan.am>
2016-04-22 13:18:48 +0400

    animation added, colors changed slightly

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    /*does not work*/
+    $('#test').on('click', function () {
+        $('#target').hide("slow");
+    })
+    //$('.parentList').each(function (id, element) {
+    //    $(element).click(function () {
+    //        this.children('ul').show('slow');
+
+
+    //    })
+    //})
+
+
+
+});
