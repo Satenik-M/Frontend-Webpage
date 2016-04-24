f5235ec7aee3ae0304eb9e398093d42e9ecb4b2b
Satenik-M <satenik@markosyan.am>
2016-04-24 20:30:21 +0400

    dropdown menu with javascript added

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-$(document).ready(function () {
-    /*does not work*/
-    $('#test').on('click', function () {
-        $('#target').hide("slow");
-    })
-    //$('.parentList').each(function (id, element) {
-    //    $(element).click(function () {
-    //        this.children('ul').show('slow');
+window.onload = function () {
+
+    var openMe = Array.prototype.slice.call(document.getElementsByClassName("open"));
+    var count = 0;
+    openMe.forEach(function (item) {
+        item.addEventListener('click', function () {
+            if (count === 0) {
+                item.lastElementChild.style.display = "block";
+                count = 1;
+            }
+            else {
+                item.lastElementChild.style.display = "none";
+                count = 0;
+            }
+        });
+    });
+
+    var link = document.createElement("link");
+    link.href = "otherStyle.css";
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    document.head.appendChild(link);
+};
 
 
-    //    })
-    //})
+//$('.parentList').each(function (id, element) {
+//    $(element).click(function () {
+//        this.children('ul').show('slow');
+
+
+//    })
+//})
 
 
 
-});
+//});
